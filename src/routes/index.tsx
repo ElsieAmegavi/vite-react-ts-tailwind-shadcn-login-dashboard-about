b0283aedd032e976a84c5b30628e386b066d0c5e
George Padmore Yeboah <34770919+GeorgePadmore@users.noreply.github.com>
2025-02-08 20:16:27 +0000

    project fully configured and works

---
+++ src/routes/index.tsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from "react-router-dom"
+import App from "../App"
+import { Home } from "../pages/Home"
+import { About } from "../pages/About"
+import { Login } from "../pages/Login"
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+    ],
+  },
+]) 
